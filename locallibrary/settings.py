17eaf57bd5f15d6c10437548d5f61af887995b08
windrangers <67287400+windrangers@users.noreply.github.com>
2020-11-25 15:48:53 +0000

    Update for heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -21,14 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vbaw&teofiji5i-6b!bf(foxtopk53w2p@(_u3=bgam0++mow&'
-
+# SECRET_KEY = 'vbaw&teofiji5i-6b!bf(foxtopk53w2p@(_u3=bgam0++mow&'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'vbaw&teofiji5i-6b!bf(foxtopk53w2p@(_u3=bgam0++mow&')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['shrouded-sierra-13892.herokuapp.com']
 
 
 # Application definition
